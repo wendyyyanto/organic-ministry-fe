07735eec9884d1acd85b696799b76fee50b64def
wendyyyanto <wendyyyanto@gmail.com>
2025-05-22 12:38:58 +0700

    enhance: update teachings page layout with video background and directory sections

--- app/(pages)/teachings/page.tsx
+++ app/(pages)/teachings/page.tsx
@@ -4,11 +4,36 @@ const TeachingsPage = () => {
 	const teachings = teachingRecordings;
 
 	return (
-		<div className="w-screen h-screen flex flex-col items-center justify-center pt-36 pb-12 px-12">
-			<h1 className="absolute top-32 text-6xl font-bold uppercase text-center w-min">
-				Teaching Recordings
-			</h1>
-			<div className="flex gap-10 mt-16 w-full h-full bg-secondary"></div>
+		<div className="pb-20 px-12">
+			<div className="w-full h-screen flex flex-col items-center justify-center pt-36 pb-12 trigger-background">
+				<h1 className="absolute top-32 text-8xl z-50 font-bold uppercase text-center w-min">
+					Teaching Recordings
+				</h1>
+				<div className="relative pt-28 w-full h-full">
+					<video
+						className="w-full h-full object-cover brightness-50 saturate-50"
+						autoPlay
+						loop
+						muted
+					>
+						<source src="/assets/videos/banner_video.mp4" />
+					</video>
+				</div>
+			</div>
+			<div className="mt-40 flex gap-10 w-full h-full">
+				<h1 className="font-bold text-6xl">Directories</h1>
+				<div className="grid grid-cols-4 gap-10 flex-1">
+					<div className="h-[170px] w-full bg-primary"></div>
+					<div className="h-[170px] w-full bg-primary"></div>
+					<div className="h-[170px] w-full bg-primary"></div>
+					<div className="h-[170px] w-full bg-primary"></div>
+					<div className="h-[170px] w-full bg-primary"></div>
+					<div className="h-[170px] w-full bg-primary"></div>
+					<div className="h-[170px] w-full bg-primary"></div>
+					<div className="h-[170px] w-full bg-primary"></div>
+					<div className="h-[170px] w-full bg-primary"></div>
+				</div>
+			</div>
 		</div>
 	);
 };

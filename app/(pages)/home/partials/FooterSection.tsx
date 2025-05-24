18f4545e32e8f4c79b5aa58823c337f28ea56e50
wendyyyanto <wendyyyanto@gmail.com>
2025-05-24 17:27:09 +0700

    wip: home page responsiveness

--- app/(pages)/home/partials/FooterSection.tsx
+++ app/(pages)/home/partials/FooterSection.tsx
@@ -3,7 +3,7 @@ function FooterSection() {
 		<div className="w-full bg-primary px-12 pt-12 text-secondary flex flex-col h-[400px] items-center justify-between">
 			<div className="w-full flex justify-between items-center">
 				<div className="flex flex-col gap-10">
-					<h1 className="text-8xl font-bold">
+					<h1 className="text-8xl max-xl:text-6xl font-bold">
 						ORGANIC <br /> MINISTRY.
 					</h1>
 				</div>
